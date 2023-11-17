db4e2f3cec3fedc797ab80acedc72505dd369d75
MizutamariShutaro <shutaro0415@outlook.jp>
2023-11-18 03:36:14 +0900

    [fix] ガチャリザルトのSkip処理修正

--- Assets/Script/Gacha/GachaTopScreen.cs
+++ Assets/Script/Gacha/GachaTopScreen.cs
@@ -1,85 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using System;
-using Cysharp.Threading.Tasks;
-using System.Threading;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using System;
+//using Cysharp.Threading.Tasks;
+//using System.Threading;
 
-//日本語対応
-public class GachaTopScreen : MonoBehaviour, IScreenController
-{
-    [SerializeField]
-    private Button _showCheckGachaButton = default;
+////日本語対応
+//public class GachaTopScreen : MonoBehaviour, IScreenController
+//{
+//    [SerializeField]
+//    private Button _showCheckGachaButton = default;
 
-    [SerializeField]
-    private Button _doGachaButton = default;
+//    [SerializeField]
+//    private Button _doGachaButton = default;
 
-    [SerializeField]
-    private GameObject _gachaStagingScreen = default;
+//    [SerializeField]
+//    private GameObject _gachaStagingScreen = default;
 
-    [SerializeField]
-    private GameObject _gachaResultScreen = default;
+//    [SerializeField]
+//    private GameObject _gachaResultScreen = default;
 
-    [SerializeField]
-    private Image _checkDoGachaPanel = default;
+//    [SerializeField]
+//    private Image _checkDoGachaPanel = default;
 
-    [SerializeField]
-    private LoadAssetData _loadAssetData = default;
+//    [SerializeField]
+//    private LoadAssetData _loadAssetData = default;
 
-    private CancellationTokenSource _cts = default;
+//    private CancellationTokenSource _cts = default;
 
 
-    private void OnEnable()
-    {
-        Initialize();
-    }
-    private void OnDisable()
-    {
-        Release();
-        _checkDoGachaPanel.gameObject.SetActive(false);
-    }
-    public void Initialize()
-    {
-        _cts = new CancellationTokenSource();
-        _checkDoGachaPanel.gameObject.SetActive(false);
-        _gachaStagingScreen.gameObject.SetActive(false);
-        gameObject.SetActive(true);
+//    private void OnEnable()
+//    {
+//        Initialize();
+//    }
+//    private void OnDisable()
+//    {
+//        Release();
+//        _checkDoGachaPanel.gameObject.SetActive(false);
+//    }
+//    public void Initialize()
+//    {
+//        _cts = new CancellationTokenSource();
+//        _checkDoGachaPanel.gameObject.SetActive(false);
+//        _gachaStagingScreen.gameObject.SetActive(false);
+//        gameObject.SetActive(true);
 
-        Subscribe();
-    }
-    public void Subscribe()
-    {
-        _showCheckGachaButton.onClick.AddListener(ShowCheckGachaPanel);
-        _doGachaButton.onClick.AddListener(() => GoNext());
-    }
+//        Subscribe();
+//    }
+//    public void Subscribe()
+//    {
+//        _showCheckGachaButton.onClick.AddListener(ShowCheckGachaPanel);
+//        _doGachaButton.onClick.AddListener(() => GoNext());
+//    }
 
-    public void Release()
-    {
-        _showCheckGachaButton.onClick.RemoveAllListeners();
-        _doGachaButton.onClick.RemoveAllListeners();
-        _cts?.Cancel();
-    }
+//    public void Release()
+//    {
+//        _showCheckGachaButton.onClick.RemoveAllListeners();
+//        _doGachaButton.onClick.RemoveAllListeners();
+//        _cts?.Cancel();
+//    }
 
-    public void BackPrevious()
-    {
+//    public void BackPrevious()
+//    {
 
-    }
-    private void ShowCheckGachaPanel()
-    {
-        _checkDoGachaPanel.gameObject.SetActive(true);
-    }
+//    }
+//    private void ShowCheckGachaPanel()
+//    {
+//        _checkDoGachaPanel.gameObject.SetActive(true);
+//    }
 
-    public async void GoNext()
-    {
-        gameObject.SetActive(false);
-        _gachaStagingScreen.SetActive(true);
-        await _loadAssetData.DataPreparation();
-        await UniTask.CompletedTask;
-    }
+//    public async void GoNext()
+//    {
+//        gameObject.SetActive(false);
+//        _gachaStagingScreen.SetActive(true);
+//        await _loadAssetData.DataPreparation();
+//        await UniTask.CompletedTask;
+//    }
 
-    void IScreenController.GoNext()
-    {
-        throw new NotImplementedException();
-    }
-}
+//    void IScreenController.GoNext()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
